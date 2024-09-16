7a7db7c1db544516b72805e9cf7a19e09bbe86a6
Joachim Oforma, Igwebuike <joachimigwebuike@gmail.com>
2024-09-16 00:47:59 -0800

    Groove Kitchen Project

---
+++ instance/config.py
@@ -0,0 +1,15 @@
+ADMIN_EMAIL = 'joachim@gmail.com'
+APP_NAME = 'grooveApp'
+DEBUG = True
+ENV = 'development'
+SECRET_KEY = 'iam/number4&'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root@localhost:3307/groovekitchendb'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+MAIL_SERVER="smtp.gmail.com"
+MAIL_PORT = 465
+MAIL_USERNAME = "larryaangjo@gmail.com"
+MAIL_PASSWORD = "jfev hxop ixlb csxu"
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
